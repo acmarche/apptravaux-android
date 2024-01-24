e6a5ec5727f1ee00cf6f95a09dc7799878d40066
jfsenechal <jfsenechal@gmail.com>
2024-01-24 17:20:47 +0100

    update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id ("kotlin-kapt")
-    id ("com.google.devtools.ksp")
-    id ("dagger.hilt.android.plugin")
-    id ("com.google.gms.google-services")
-    id ("com.google.firebase.crashlytics")
+    //kotlin("kapt") version "1.9.22"
+    id("com.google.devtools.ksp")
+    id("dagger.hilt.android.plugin")
+    id("com.google.gms.google-services")
+    id("com.google.firebase.crashlytics")
 }
 
 android {
@@ -16,7 +16,7 @@ android {
         applicationId = "be.marche.apptravaux"
         minSdk = 30//v11
         targetSdk = 34
-        versionCode =29
+        versionCode = 29
         versionName = "1.4.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -59,16 +59,112 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
-    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    implementation(platform("androidx.compose:compose-bom:2023.10.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
+    //implementation("androidx.compose.material3:material3")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.10.01"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    val materialVersion = "1.5.4"
+    implementation("androidx.compose.material:material:$materialVersion")
+    /**
+     * viewmodel in compose
+     */
+    implementation("androidx.fragment:fragment-ktx:1.6.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+
+    /**
+     * Using hilt for DI
+     */
+    implementation("com.google.dagger:hilt-android:2.50")
+    //kapt("com.google.dagger:hilt-android-compiler:2.50")
+    ksp("com.google.dagger:dagger-compiler:2.50") // Dagger compiler
+    ksp("com.google.dagger:hilt-compiler:2.50")   // Hilt compiler
+    //ksp("androidx.hilt:hilt-compiler:1.1.0")
+    // Allow references to generated code
+    //kapt {
+    //     correctErrorTypes = true
+    // }
+
+    /**
+     * My networking stuff cause Retrofit is da best.
+     */
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+
+    /**
+     * coroutines for async operations
+     */
+    val lifecycleVersionKt = "2.7.0"
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersionKt")
+
+    /**
+     * Room
+     */
+    val roomVersion = "2.6.1"
+    implementation("androidx.room:room-runtime:$roomVersion")
+    annotationProcessor("androidx.room:room-compiler:$roomVersion")
+
+    ksp("androidx.room:room-compiler:$roomVersion")
+    // optional - Kotlin Extensions and Coroutines support for Room
+    implementation("androidx.room:room-ktx:$roomVersion")
+
+    /**
+     * Navigation
+     */
+    implementation("androidx.navigation:navigation-compose:2.7.6")
+    /**
+     * Tools
+     */
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    //ksp("com.github.bumptech.glide:compiler:4.16.0")
+    implementation("io.coil-kt:coil-compose:2.5.0")
+    val timberVersion = "5.0.1"
+    implementation("com.jakewharton.timber:timber:$timberVersion")
+
+    /**
+     * Firebase
+     */
+    implementation(platform("com.google.firebase:firebase-bom:32.7.1"))
+    implementation("com.google.firebase:firebase-crashlytics-ktx")
+    implementation("com.google.firebase:firebase-analytics-ktx")
+
+    /**
+     * Permission
+     */
+    implementation("com.google.accompanist:accompanist-permissions:0.32.0")
+
+    /**
+     * Google Map
+     */
+    implementation("com.google.maps.android:maps-compose:4.3.1")
+    implementation("com.google.android.gms:play-services-location:21.1.0")
+    // Make sure to also include the latest version of the Maps SDK for Android
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
+    implementation("androidx.localbroadcastmanager:localbroadcastmanager:1.1.0")
+    /**
+     * work manager
+     */
+    val workVersion = "2.9.0"
+    implementation("androidx.work:work-runtime:$workVersion")
+    implementation("androidx.work:work-runtime-ktx:$workVersion")
+    implementation("androidx.hilt:hilt-work:1.1.0")
+    // optional - GCMNetworkManager support
+    implementation("androidx.work:work-gcm:$workVersion")
+    //app start up
+    implementation("androidx.startup:startup-runtime:1.1.1")
+
+    //FlowLayout
+    implementation("com.google.accompanist:accompanist-flowlayout:0.32.0")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -4,9 +4,8 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     
     //??id ("com.android.library") version "8.2.2" apply false
-    
-    id ("com.google.dagger.hilt.android") version "2.42" apply false
-    id ("com.google.gms.google-services") version "4.3.14" apply false
-    id ("com.google.firebase.crashlytics") version "2.8.1" apply false
-    id ("com.google.devtools.ksp") version "1.8.10-1.0.9" apply false
+    id("com.google.devtools.ksp") version "1.9.0-1.0.12" apply false
+    id("com.google.dagger.hilt.android") version "2.42" apply false
+    id("com.google.gms.google-services") version "4.3.14" apply false
+    id("com.google.firebase.crashlytics") version "2.8.1" apply false
 }
