3acebd795992a0b0341932a8649a1db8fe17d175
jfsenechal <jfsenechal@gmail.com>
2024-02-21 13:19:08 +0100

    remove OutlinedButtonJf

--- app/src/main/java/be/marche/apptravaux/screens/widgets/widgets.kt
+++ app/src/main/java/be/marche/apptravaux/screens/widgets/widgets.kt
@@ -4,16 +4,33 @@ import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.material.Card
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Icon
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.ProgressIndicatorDefaults
+import androidx.compose.material.Text
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
@@ -130,22 +147,6 @@ fun OutlinedTextFieldJf(textStateThree: MutableState<TextFieldValue>) {
             backgroundColor = Color.Transparent
         )
     )
-}
-
-@Composable
-fun OutlinedButtonJf(texte: String, isEnabled: Boolean, onItemCLick: () -> Unit) {
-    OutlinedButton(
-        onClick = onItemCLick,
-        shape = CircleShape,
-        enabled = isEnabled,
-        elevation = ButtonDefaults.elevation(8.dp),
-        // modifier = Modifier.fillMaxWidth(0.5f)
-    ) {
-        Text(
-            text = texte,
-            modifier = Modifier.padding(6.dp)
-        )
-    }
 }
 
 @Composable
